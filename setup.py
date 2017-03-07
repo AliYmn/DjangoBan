520e8c558ff741220e35eff0aebfbb35414208c0
aliyaman <aliymn.db@gmail.com>
2017-03-08 00:53:56 +0300

    setup.pu version update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DjangoBan',
-    version='1.8.0',
+    version='1.9.0',
     packages=['ban', 'ban.migrations'],
     url='https://github.com/AliYmn/DjangoBan',
     license='MIT',
